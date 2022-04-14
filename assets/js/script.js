faa9d6151d29c7e2a998df7570d28dcdd078aade
Christopher <leechrs1@gmail.com>
2022-04-13 20:35:02 -0500

    working on JS

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+var score = 0;
+var time = 10;
+var startBtn = document.getElementById('startBtn');
+var pokemonId = 1;
+var sprite = 1
+var apiUrl = "https://pokeapi.co/api/v2/pokemon/" + pokemonId;
+
+// fetch(apiUrl).then(function(response){
+//     response.json().then(function(data){
+//         console.log(data.name);
+//         console.log(data.sprites.front_shiny)
+//     });
+// });
+
+startBtn.addEventListener("click", function(){
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            console.log(data.name);
+            console.log(data.sprites.front_shiny);
+            var name = data.name;
+            var pokeSprite = data.sprites.front_shiny;
+            document.querySelector('video').style.visibility='hidden';
+            document.querySelector('image').innerHTML=`<img src = ${pokeSprite}>`
+        }); 
+    });
+})
+
+
+
+
